a125e8c6138af79a538274efa455fc2fb13a3a5c
ELIEDSON GUSTAVO <83400143+ELIEDSON-GUSTAVO@users.noreply.github.com>
2023-07-29 16:15:35 -0300

    push

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let isSoundEnabled = false;
 
 playIcon.addEventListener("click", function () {
   if (!isSoundEnabled) {
-      backgroundVideo.muted = false;
-          isSoundEnabled = true;
-            }
-            });
+  backgroundVideo.muted = false;
+  isSoundEnabled = true;
+}
+});
             
