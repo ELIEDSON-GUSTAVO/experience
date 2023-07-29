5575e3a5dbf65d326e75a0ce8249ec559128b894
ELIEDSON GUSTAVO <83400143+ELIEDSON-GUSTAVO@users.noreply.github.com>
2023-07-29 17:24:46 -0300

    push

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const video = document.getElementById("video");
+const backgroundVideo = document.getElementById("backgroundVideo");
 const playIcon = document.getElementById("playIcon");
+const iconLink = document.getElementById("iconLink");
 
 let isSoundEnabled = false;
 
-playIcon.addEventListener("click", function () {
-  if (!isSoundEnabled) {
-      video.muted = false;
-      isSoundEnabled = true;
+iconLink.addEventListener("click", function (event) {
+  event.preventDefault(); // Impede o comportamento padrão do link
+    if (!isSoundEnabled) {
+        backgroundVideo.muted = false;
+        isSoundEnabled = true;
 }
 });
