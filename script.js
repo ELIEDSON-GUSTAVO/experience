26637d07f83ff16227a42084dd49d7e6ac0433ac
ELIEDSON GUSTAVO <83400143+ELIEDSON-GUSTAVO@users.noreply.github.com>
2024-03-28 20:08:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,60 +9,59 @@ let typingTitle = false;
 
 iconLink.addEventListener("click", function (event) {
     event.preventDefault(); // Impede o comportamento padrão do link
-        if (!isSoundEnabled) {
-                backgroundVideo.muted = false;
-                        backgroundVideo.volume = 0.5; // Defina o volume desejado entre 0.0 e 1.0
-                                backgroundVideo.play();
-                                        isSoundEnabled = true;
-                                            } else {
-                                                    backgroundVideo.volume = 0; // Define o volume do áudio como 0 (mudo)
-                                                            isSoundEnabled = false;
-                                                                }
-                                                                });
+    if (!isSoundEnabled) {
+        backgroundVideo.muted = false;
+        backgroundVideo.volume = 0.5; // Defina o volume desejado entre 0.0 e 1.0
+        backgroundVideo.play();
+        isSoundEnabled = true;
+    } else {
+        backgroundVideo.volume = 0; // Define o volume do áudio como 0 (mudo)
+        isSoundEnabled = false;
+    }
+});
 
-                                                                whatsappLink.addEventListener("click", function (event) {
-                                                                    event.preventDefault(); // Impede o comportamento padrão do link
-                                                                        window.location.href = "https://wa.me/message/CQMJ7TPPC6XAL1";
-                                                                        });
+whatsappLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    window.location.href = "https://wa.me/message/CQMJ7TPPC6XAL1";
+});
 
-                                                                        titles[0].addEventListener("input", function () {
-                                                                            checkUserInput();
-                                                                            });
+titles[0].addEventListener("input", function () {
+    checkUserInput();
+});
 
-                                                                            function typeTitle() {
-                                                                                const fullTitle = titles[currentTitleIndex].textContent.trim();
-                                                                                    let currentLength = 0;
-                                                                                        const titleElement = titles[currentTitleIndex];
+function typeTitle() {
+    const fullTitle = titles[currentTitleIndex].textContent.trim();
+    let currentLength = 0;
+    const titleElement = titles[currentTitleIndex];
 
-                                                                                            function type() {
-                                                                                                    titleElement.textContent = fullTitle.slice(0, currentLength);
-                                                                                                            currentLength++;
+    function type() {
+        titleElement.textContent = fullTitle.slice(0, currentLength);
+        currentLength++;
 
-                                                                                                                    if (currentLength <= fullTitle.length) {
-                                                                                                                                setTimeout(type, 50); // Ajuste a velocidade da digitação alterando o valor (em milissegundos)
-                                                                                                                                        } else {
-                                                                                                                                                    typingTitle = false;
-                                                                                                                                                                titleElement.classList.remove("cursor-animation");
-                                                                                                                                                                            currentTitleIndex++;
-                                                                                                                                                                                        if (currentTitleIndex < titles.length) {
-                                                                                                                                                                                                        typeTitle();
-                                                                                                                                                                                                                    } else {
-                                                                                                                                                                                                                                    checkUserInput();
-                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                            }
+        if (currentLength <= fullTitle.length) {
+            setTimeout(type, 50); // Ajuste a velocidade da digitação alterando o valor (em milissegundos)
+        } else {
+            typingTitle = false;
+            titleElement.classList.remove("cursor-animation");
+            currentTitleIndex++;
+            if (currentTitleIndex < titles.length) {
+                typeTitle();
+            } else {
+                checkUserInput();
+            }
+        }
+    }
 
-                                                                                                                                                                                                                                                                typingTitle = true;
-                                                                                                                                                                                                                                                                    titleElement.classList.add("cursor-animation");
-                                                                                                                                                                                                                                                                        type();
-                                                                                                                                                                                                                                                                        }
+    typingTitle = true;
+    titleElement.classList.add("cursor-animation");
+    type();
+}
 
-                                                                                                                                                                                                                                                                        function checkUserInput() {
-                                                                                                                                                                                                                                                                            const userText = titles[0].textContent.trim().toLowerCase();
-                                                                                                                                                                                                                                                                                if (userText === "sim") {
-                                                                                                                                                                                                                                                                                        window.location.href = "https://wa.me/message/CQMJ7TPPC6XAL1";
-                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                            }
+function checkUserInput() {
+    const userText = titles[0].textContent.trim().toLowerCase();
+    if (userText === "sim") {
+        window.location.href = "https://wa.me/message/CQMJ7TPPC6XAL1";
+    }
+}
 
-                                                                                                                                                                                                                                                                                            typeTitle(); // Iniciar a animação de escrita do primeiro título
-                                                                                                                                                                                                                                                                                            
+typeTitle(); // Iniciar a animação de escrita do primeiro título
